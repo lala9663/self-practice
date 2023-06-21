9894f189f882258eed0cf311d3441c01cd2094d4
lala9663 <lala96632040@gmail.com>
2023-06-21 21:34:11 +0900

    Add 'summerNote/' from commit '002c2d4011dada65f87afbe57b2eddc35de7a91f'
    git-subtree-dir: summerNote
    git-subtree-mainline: 002c2d4011dada65f87afbe57b2eddc35de7a91f
    git-subtree-split: 002c2d4011dada65f87afbe57b2eddc35de7a91f

---
+++ summerNote/src/main/java/com/example/summernote/SummerNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.summernote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerNoteApplication.class, args);
+	}
+
+}
